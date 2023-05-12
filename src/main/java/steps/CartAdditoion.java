594d3fdca48b8b03b40de4c431b0c18693911132
lekshmikjayan <lekshmikjayan27@gmail.com>
2023-05-12 16:06:22 -0600

    Providing constructors and initialising new driver in POM classes and step definitions

--- src/main/java/steps/CartAdditoion.java
+++ src/main/java/steps/CartAdditoion.java
@@ -10,33 +10,37 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class CartAdditoion extends DriverInstance{
-
+public class CartAdditoion {
+	DriverInstance newdriver;
+	
+	public CartAdditoion(DriverInstance newdriver) {
+		this.newdriver = newdriver;
+	}
 	
 
 	@Given("user login into app with {string} and {string}")
 	public void userLoginIntoAppWithAnd(String uname, String pwd) {
-		driver.findElement(By.id("mat-input-2")).sendKeys(uname);
-		driver.findElement(By.id("mat-input-3")).sendKeys(pwd);
+		newdriver.getDriver().findElement(By.id("mat-input-2")).sendKeys(uname);
+		newdriver.getDriver().findElement(By.id("mat-input-3")).sendKeys(pwd);
 	}
 
 	@Given("user search for  {string}")
 	public void userSearchFor(String book) {
-		driver.findElement(By.cssSelector("input[placeholder=\"Search books or authors\"]")).sendKeys(book);
-		driver.findElement(By.cssSelector("mat-option[role='option'] span")).click();
+		newdriver.getDriver().findElement(By.cssSelector("input[placeholder=\"Search books or authors\"]")).sendKeys(book);
+		newdriver.getDriver().findElement(By.cssSelector("mat-option[role='option'] span")).click();
 	}
 
 	@When("user add book to cart")
 	public void userAddBookToCart() {
-		WebElement addBtn = driver.findElement(By.cssSelector("button[color=\"primary\"]"));
-		WebElement optn = wait.until(ExpectedConditions.visibilityOf(addBtn));
+		WebElement addBtn = newdriver.getDriver().findElement(By.cssSelector("button[color=\"primary\"]"));
+		WebElement optn = newdriver.getwait().until(ExpectedConditions.visibilityOf(addBtn));
 		optn.click();
 	}
 
 	@Then("cart badge updated")
 	public void cartBadgeUpdated() {
-		String num =	driver.findElement(By.id("mat-badge-content-0")).getText();
+		String num =	newdriver.getDriver().findElement(By.id("mat-badge-content-0")).getText();
 		Assert.assertEquals(Integer.parseInt(num) > 0, true);
-		driver.quit();
+		newdriver.getDriver().quit();
 	}
 }
--- src/main/java/steps/signin.java
+++ src/main/java/steps/signin.java
@@ -10,35 +10,39 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class signin extends DriverInstance {
+public class signin  {
+	DriverInstance newdriver;
 	
+	public signin(DriverInstance newdriver) {
+		this.newdriver = newdriver;
+	}
 
 	@And("user enters {string} as username")
 	public void userEntersAsUsername(String username) {
-		driver.findElement(By.id("mat-input-2")).sendKeys(username);
+		newdriver.getDriver().findElement(By.id("mat-input-2")).sendKeys(username);
 	}
 
 	@And("user enetrs {string} as password")
 	public void userEnetrsAsPassword(String password) {
-		driver.findElement(By.id("mat-input-3")).sendKeys(password);
+		newdriver.getDriver().findElement(By.id("mat-input-3")).sendKeys(password);
 	}
 
 	@When("user clicks login button")
 	public void userClicksLoginButton() {
-		driver.findElement(By.cssSelector("body.app-background:nth-child(2) div.container app-login.ng-star-inserted:nth-child(2) div.docs-example-viewer-wrapper div.docs-example-viewer-body div.full-width mat-card.mat-card.mat-focus-indicator.mat-elevation-z4 mat-card-content.mat-card-content form.ng-dirty.ng-touched.ng-valid mat-card-actions.mat-card-actions:nth-child(3) > button.mat-focus-indicator.mat-raised-button.mat-button-base.mat-primary:nth-child(2)")).click();
+		newdriver.getDriver().findElement(By.cssSelector("body.app-background:nth-child(2) div.container app-login.ng-star-inserted:nth-child(2) div.docs-example-viewer-wrapper div.docs-example-viewer-body div.full-width mat-card.mat-card.mat-focus-indicator.mat-elevation-z4 mat-card-content.mat-card-content form.ng-dirty.ng-touched.ng-valid mat-card-actions.mat-card-actions:nth-child(3) > button.mat-focus-indicator.mat-raised-button.mat-button-base.mat-primary:nth-child(2)")).click();
 	}
 
 	@Then("Signin successfully passed")
 	public void signinSuccessfullyPassed() {
-		WebElement text = 	driver.findElement(By.cssSelector("input[placeholder=\"Search books or authors\"]"));
-		wait.until(ExpectedConditions.visibilityOf(text));
+		WebElement text = 	newdriver.getDriver().findElement(By.cssSelector("input[placeholder=\"Search books or authors\"]"));
+		newdriver.getwait().until(ExpectedConditions.visibilityOf(text));
 		Assert.assertEquals(text.isDisplayed(), true);
 
 	}
 
 	@Then("signing failed")
 	public void signing_failed() {
-		String textt =  driver.findElement(By.id("mat-error-0")).getText();
+		String textt =  newdriver.getDriver().findElement(By.id("mat-error-0")).getText();
 		Assert.assertEquals(textt.trim(), "Username or Password is incorrect.");
 
 	}
